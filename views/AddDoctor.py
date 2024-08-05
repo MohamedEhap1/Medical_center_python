2555b5d7789c504d5ad557cdc0a5a767b7332a83
Mohamed Ehab <mohamedehap172004@gmail.com>
2024-08-05 16:39:33 +0300

    Add Doctor Update Ui

--- views/AddDoctor.py
+++ views/AddDoctor.py
@@ -42,8 +42,8 @@ def reset_frame():
     date_ent.delete(0, "end")
     date_ent.insert(0, date.today().strftime('%Y-%m-%d'))
     date_ent.config(state='readonly')
-    combo_stat.set('Select Status')
-    combo1_stat.set('Select Bloodtype')
+    combo_stat.set('Medical Specialty')
+    # combo1_stat.set('Select Bloodtype')
     radio_var.set(1)
 
 # Screen Center Function
@@ -58,7 +58,7 @@ def center_screen(root, w, h):
 
 #Button Add command
 def checker():
-    if user_ent.get() !='Enter Doctor name' and phone_ent.get() != 'Enter Doctor phone' and id_ent.get()!='Enter Doctor ID' and age_ent.get()!='Enter Docotr Age' and combo_stat.get()!='Medical Specialty' and combo1_stat.get()!='Select Bloodtype' :
+    if user_ent.get() !='Enter Doctor name' and phone_ent.get() != 'Enter Doctor phone' and id_ent.get()!='Enter Doctor ID' and age_ent.get()!='Enter Doctor Age' and combo_stat.get()!='Select Status':
         messagebox.showinfo("Succeful", "Data added succefully.")
         reset_frame()
     else:
@@ -67,7 +67,7 @@ def checker():
 
 # Initialize Tkinter root widget and title
 root = Tk()
-root.title("Add Doctor")
+root.title("Add Patient")
 font1 = ('times new roman', 14, 'bold')  # Updated font to bold
 
 # Style configuration for the radiobuttons
@@ -81,10 +81,10 @@ style.map("TRadiobutton",
         foreground=[('selected', 'black')])
 
 # Icon Image
-root.iconbitmap("images/patient.ico")
+root.iconbitmap("images/Patient.ico")
 
 # Background Image
-img_path = "images/Add Doctor.png"  # Use raw string for Windows path
+img_path = "images/Add Patient.png"  # Use raw string for Windows path
 img = Image.open(img_path)
 img = img.resize((1500, 750))
 
@@ -99,7 +99,7 @@ bglabel.place(relwidth=1, relheight=1)  # Make label fill the whole window
 center_screen(root, 1500, 750)
 root.resizable(False, False)
 
-# User (Doctor name) Icon
+# User (Doctorname) Icon
 usericon_path = "images/Usericon.png"  # Use raw string for Windows path
 Userimg = Image.open(usericon_path)
 Userimg = Userimg.resize((80, 80))
@@ -107,7 +107,7 @@ Userimg_tk = ImageTk.PhotoImage(Userimg)
 icon_label = Label(root, image=Userimg_tk, relief='flat', borderwidth=0, highlightthickness=0)
 icon_label.place(x=220, y=130)
 
-# Doctor name entry
+# Doctorname entry
 user_ent = PlaceholderEntry(root, placeholder="Enter Doctor name", font=font1)
 user_ent.place(x=300, y=155, width=200, height=30)
 
@@ -144,7 +144,7 @@ rbtn1.place(x=1050, y=274, width=100, height=30)
 rbtn2.place(x=1190, y=274, width=100, height=30)
 
 
-#Doctor Id icon
+#DoctorId icon
 idicon_path = "images/Id.png"  # Use raw string for Windows path
 idimg = Image.open(idicon_path)
 idimg = idimg.resize((80, 80))
@@ -152,11 +152,11 @@ idimg_tk = ImageTk.PhotoImage(idimg)
 icon_label = Label(root, image=idimg_tk, relief='flat', borderwidth=0, highlightthickness=0)
 icon_label.place(x=220, y=250)
 
-#Doctor Id entry
+#DoctorId entry
 id_ent = PlaceholderEntry(root, placeholder="Enter Doctor ID", font=font1)
 id_ent.place(x=300, y=274, width=200, height=30)
 
-#Doctor Age icon
+#DoctorAge icon
 ageicon_path = "images/Age.png"  # Use raw string for Windows path
 ageimg = Image.open(ageicon_path)
 ageimg = ageimg.resize((80, 80))
@@ -164,11 +164,11 @@ ageimg_tk = ImageTk.PhotoImage(ageimg)
 phone_icon_label = Label(root, image=ageimg_tk, relief='flat', borderwidth=0, highlightthickness=0)
 phone_icon_label.place(x=620, y=250)
 
-#Doctor Age entry
+#DoctorAge entry
 age_ent = PlaceholderEntry(root, placeholder="Enter Doctor Age", font=font1)
 age_ent.place(x=700, y=274, width=200, height=30)
 
-#Doctor status icon
+#Doctorstatus icon
 staticon_path = "images/status.png"  # Use raw string for Windows path
 statimg = Image.open(staticon_path)
 statimg = statimg.resize((80, 80))
@@ -176,13 +176,13 @@ statimg_tk = ImageTk.PhotoImage(statimg)
 phone_icon_label = Label(root, image=statimg_tk, relief='flat', borderwidth=0, highlightthickness=0)
 phone_icon_label.place(x=220, y=374)
 
-#Doctor status combo box
+#Doctorstatus combo box
 combo_stat_var = StringVar()
 combo_stat_var.set('Medical Specialty')
 combo_stat = Combobox(root, values=('Allergy and Immunology', 'Anesthesiology', 'Cardiology', 'Dermatology', 'Emergency Medicine', 'Family Medicine', 'Gastroenterology', 'Geriatrics', 'Hematology', 'Infectious Disease', 'Internal Medicine', 'Medical Genetics', 'Nephrology', 'Neurology', 'Neurosurgery', 'Obstetrics and Gynecology', 'Oncology', 'Ophthalmology', 'Orthopedic Surgery', 'Otolaryngology', 'Pathology', 'Pediatrics', 'Physical Medicine and Rehabilitation', 'Plastic Surgery', 'Preventive Medicine', 'Psychiatry', 'Pulmonology', 'Radiology', 'Surgery', 'Urology'), textvariable=combo_stat_var, state='readonly')
 combo_stat.place(x=300,y=400,width=200,height=30)
 
-#Doctor bloodType icon
+#DoctorbloodType icon
 # bloodicon_path = "images/BloodType.png"  # Use raw string for Windows path
 # bloodimg = Image.open(bloodicon_path)
 # bloodimg = bloodimg.resize((80, 80))
